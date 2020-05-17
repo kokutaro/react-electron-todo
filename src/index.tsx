aa4360c504f0def5c8c0141e3516b4eb98c51763
Hiromi Kohara <h_kohara@me.com>
2020-05-17 16:15:32 -0700

    day two

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('contents');
+
+ReactDOM.render(<p>Hello, world;</p>, container);
